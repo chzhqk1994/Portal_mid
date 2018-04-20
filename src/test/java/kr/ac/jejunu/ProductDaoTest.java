def2a219327e59aa2460786ad110ec8b67f109f4
hyunwoo <chzhqk1994@gmail.com>
2018-04-20 15:18:21 +0900

    1

--- src/test/java/kr/ac/jejunu/ProductDaoTest.java
+++ src/test/java/kr/ac/jejunu/ProductDaoTest.java
@@ -1,5 +1,6 @@
 package kr.ac.jejunu;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.SQLException;
@@ -7,12 +8,15 @@ import java.sql.SQLException;
 import static org.junit.Assert.assertEquals;
 
 public class ProductDaoTest {
+    private ProductDao productDao;
+
+    @Before
     public void setup() {
+        productDao = new ProductDao();
     }
 
     @Test
     public void get() throws SQLException, ClassNotFoundException {
-        ProductDao productDao = new ProductDao();
         Long id = 1L;
         String title = "제주감귤";
         Integer price = 15000;
@@ -22,4 +26,18 @@ public class ProductDaoTest {
         assertEquals(title, product.getTitle());
         assertEquals(price, product.getPrice());
     }
+
+    @Test
+    public void add() throws SQLException, ClassNotFoundException {
+        Product product = new Product();
+        product.setTitle("시험");
+        product.setPrice(100);
+        Long id = productDao.insert(product);
+
+        Product insertedProduct = productDao.get(id);
+        assertEquals(insertedProduct.getId(), id);
+        assertEquals(insertedProduct.getTitle(), product.getTitle());
+        assertEquals(insertedProduct.getPrice(), product.getPrice());
+    }
+
 }
