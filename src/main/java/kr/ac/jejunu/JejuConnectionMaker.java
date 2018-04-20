55240805a30946784a6812687605e3d2332ca0b3
hyunwoo <chzhqk1994@gmail.com>
2018-04-20 15:31:40 +0900

    5

--- src/main/java/kr/ac/jejunu/JejuConnectionMaker.java
+++ src/main/java/kr/ac/jejunu/JejuConnectionMaker.java
@@ -1,13 +1,29 @@
 package kr.ac.jejunu;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JejuConnectionMaker implements ConnectionMaker {
+
+    @Value("${db.classname}")
+    private String className;
+    @Value("${db.url}")
+    private String url;
+    @Value("${db.username}")
+    private String username;
+    @Value("${db.password}")
+    private String password;
+
     @Override
     public Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?characterEncoding=utf-8", "root", "thdgusdn");
+        className = "com.mysql.jdbc.Driver";
+        Class.forName(className);
+        url = "jdbc:mysql://localhost/jeju?characterEncoding=utf-8";
+        username = "root";
+        password = "thdgusdn";
+        return DriverManager.getConnection(url, username, password);
     }
 }
